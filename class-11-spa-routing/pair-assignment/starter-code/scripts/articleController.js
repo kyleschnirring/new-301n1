47131678faa9d48bc4eca61cdb8ef2631867e37f
kyleschnirring <kyleschnirring@icloud.com>
2016-02-06 11:29:20 -0800

    added some script tags

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -7,7 +7,7 @@
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+  
   };
 
   module.articlesController = articlesController;
